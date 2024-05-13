4a6762b3e825458670924bb7310b2d06c0fd51d4
Tiago Silva <tsilvasantos38@gmail.com>
2024-05-13 14:50:20 -0300

    CI-CD: fix deploy

--- scripts/frontend/fix_files_dir.sh
+++ scripts/frontend/fix_files_dir.sh
@@ -6,4 +6,4 @@ echo "-> Deleting all files in $CURRENT_DIR except frontend directory..."
 find $CURRENT_DIR ! -name "frontend" -exec rm -rf {} +
 
 echo "-> Moving all files from frontend directory to $CURRENT_DIR..."
-mv $CURRENT_DIR/frontend/* .
+mv $CURRENT_DIR/frontend/* $CURRENT_DIR
